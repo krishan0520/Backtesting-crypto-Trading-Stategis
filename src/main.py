42c14aa4c83b6d2264d8155a1cdd74ffa254f9aa
krishan0520git config --global user.name krishan0520 <vimukthi.hut@gmail.com>
2025-02-16 18:57:54 +0000

    main1

---
+++ src/main.py
@@ -0,0 +1,21 @@
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s %(levelname)s :: %(message)s')
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+stream_handler.setLevel(logging.INFO)
+
+file_handler = logging.FileHandler('info.log')
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.DEBUG)
+
+logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
+
+if __name__=='__main__':
+
+    mode = input("Chose the program mode (data/backtest/optimize): ").lower()

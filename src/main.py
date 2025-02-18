7f11cbaaf88117bc32ab2ed8653c74609cd5566c
krishan0520git config --global user.name krishan0520 <vimukthi.hut@gmail.com>
2025-02-18 08:41:18 +0000

    python binance ex

--- src/main.py
+++ src/main.py
@@ -1,4 +1,21 @@
 import logging
+"""
+This script sets up logging for a crypto trading backtesting application and initializes a Binance client.
+Modules:
+    logging: Provides a way to configure and use loggers.
+    exchage.binance: Contains the BinanceClient class for interacting with the Binance exchange.
+Logging Configuration:
+    - Logs are formatted with timestamps, log level, and message.
+    - Logs are output to both the console (INFO level) and a file named 'info.log' (DEBUG level).
+Classes:
+    BinannceClient: A client for interacting with the Binance exchange.
+Functions:
+    main: The entry point of the script. Prompts the user to choose a program mode and initializes the Binance client.
+Usage:
+    Run the script and follow the prompt to choose a program mode (data/backtest/optimize).
+"""
+
+from exchage.binance import BinannceClient
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -19,3 +36,7 @@ logger.addHandler(file_handler)
 if __name__=='__main__':
 
     mode = input("Chose the program mode (data/backtest/optimize): ").lower()
+    
+    client = BinannceClient(True)
+
+    print(client.get_historical_data("BTCUSDT"))

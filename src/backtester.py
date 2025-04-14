ab900be533eba9ed9c33fcafd6dbf5eb33161655
krishan0520git config --global user.name krishan0520 <vimukthi.hut@gmail.com>
2025-04-14 22:40:42 +0100

    support res

--- src/backtester.py
+++ src/backtester.py
@@ -1,5 +1,6 @@
 from data_collection import Hdf5Client
 import strategies.ichmoku
+import strategies.support_res
 from utils import*
 
 import strategies.obv
@@ -23,6 +24,15 @@ def run(exchange:str,strategy:str,symbol:str,tf:str,from_time:int,to_time:int):
         data = resample_timeframe(data,tf)
 
         print(strategies.ichmoku.backtest(data,tenkan_period=9,kijun_period=26))
+
+
+    elif strategy=="sup_res":
+
+        h5_db = Hdf5Client(exchange)
+        data =h5_db.get_data(symbol,from_time,to_time)
+        data = resample_timeframe(data,tf)
+
+        print(strategies.support_res.backtest(data,min_point=3,min_diff_point=7,rounding_num=200,profit_take=3,stop_loss=3))
 
 
 
---
+++ src/strategies/support_res.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import numpy as np
+
+
+
+pd.set_option("display.max_columns",None)
+#pd.set_option("display.max_rows",None)
+pd.set_option("display.width",1000)
+
+
+def backtest(df :pd.DataFrame,min_point:int,min_diff_point :int ,rounding_num:float,profit_take:float,stop_loss:float):
+
+    candle_leg = df.iloc[1].name - df.iloc[0].name
+
+    df["rounded_high"] = round(df["high"] / rounding_num )*rounding_num
+    df["rounded_low"]  = round(df["low"] / rounding_num) * rounding_num
+
+
+    price_group = {"resistances":dict() , "supports":dict()}
+
+    support_res = {"resistances": [] , "supports": []}
+
+    for index,row in df.iterrows():
+
+        for side in ["resistances","supports"]:
+
+            h_l = "high" if side == "resistances" else "low"
+
+
+            if row["rounded_" + h_l] in price_group[side]:
+
+                grp = price_group[side][row["rounded_" + h_l]]
+
+                if index>= grp["last"] + min_diff_point*candle_leg:
+
+                    grp["price"].append(row[h_l])
+
+                    if len(grp["price"])>= min_point:
+
+                        extrem_price = max(grp["price"]) if side == "resistances" else mix(grp["price"])
+
+                        support_res[side].append({"price":extrem_price, "broken":False})
+
+                    grp["last"] = index
+
+
+
+
+
+
+            else:
+                price_group[side][row["rounded_" + h_l]] = {"price":[row[ h_l]] , "start_time":index,"last":index}
+                
+
+
+
+
+
+    
+
+
+    
